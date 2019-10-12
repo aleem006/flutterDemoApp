93ce1256eadce6a084e6f588407e9d5c43fe31ae
aleem006 <aleem.mohsan@gmail.com>
2019-10-13 01:05:45 +0200

    Name and symptoms can be enter from user and result can be visible on terminal

--- lib/main.dart
+++ lib/main.dart
@@ -22,6 +22,7 @@ class PersonData {
 }
 
 class MyHomePage extends StatefulWidget {
+
   MyHomePage({Key key, this.title}) : super(key: key);
   // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
@@ -32,17 +33,49 @@ class MyHomePage extends StatefulWidget {
   // used by the build method of the State. Fields in a Widget subclass are
   // always marked "final".
   final String title;
+  
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-String _userName, _symptoms;
-final formKey = GlobalKey<FormState>();
+// String _userName, _symptoms;
+PersonData person = PersonData();
+final _formKey = GlobalKey<FormState>();
+
   void _save() {
-   
+    final FormState form = _formKey.currentState;
+    if (!form.validate()) {
+      // _autovalidate = true; // Start validating on every change.
+      // showInSnackBar('Please fix the errors in red before submitting.');
+      print("please write proprt");
+    } else {
+      form.save();
+      print(person.name);
+      print(person.symptoms);
+    }
   }
+
+  //  String _validateName(String value) {
+  //   // _formWasEdited = true;
+  //   if (value.isEmpty)
+  //     return 'Name is required.';
+  //   final RegExp nameExp = RegExp(r'^[A-Za-z ]+$');
+  //   if (!nameExp.hasMatch(value))
+  //     return 'Please enter only alphabetical characters.';
+  //   return null;
+  // }
+
+  //  String _validateSymptom(String value) {
+  //   // _formWasEdited = true;
+  //   if (value.isEmpty)
+  //     return 'Sympotm is required.';
+  //   final RegExp nameExp = RegExp(r'^[A-Za-z ]+$');
+  //   if (!nameExp.hasMatch(value))
+  //     return 'Please enter only alphabetical characters.';
+  //   return null;
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -65,40 +98,48 @@ final formKey = GlobalKey<FormState>();
           child: Padding(
             padding: EdgeInsets.all(8.0),
             child: Form(
-              key: formKey,
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: <Widget>[
-                  TextFormField(
-                    decoration: InputDecoration(
-                        labelText: 'Name'
-                    ),
-                  ),
-                  TextFormField(
-                    decoration: InputDecoration(
-                        labelText: 'Symptoms'
-                    ),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
+              key: _formKey, //refrencing this form 
+              // child: Column(
+              child: Scrollbar(
+                child: SingleChildScrollView( 
+                  // dragStartBehavior: DragStartBehavior.down,
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
                     children: <Widget>[
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: RaisedButton(
-                          onPressed: _save,
-                          child: Text('Save'),
+                      TextFormField(
+                        decoration: InputDecoration(
+                          labelText: 'Name'
                         ),
-                      )
-                    ],
-                  )
-                ],
+                        onSaved: (String value) { person.name = value; },
+                        // validator: _validateName,
+                      ),
+
+                      TextFormField(
+                        decoration: InputDecoration(
+                          labelText: 'Symptoms'
+                        ),
+                        onSaved: (String value) { person.symptoms = value; },
+                        // validator: _validateSymptom,
+                      ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: <Widget>[
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: RaisedButton(
+                            onPressed: _save,
+                            child: Text('Save'),
+                          ),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+                ),
               ),
             ),
           ),
         ));
-          // child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
   }
 }
